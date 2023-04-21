2a2c23f1f9fbb8c9ea8016aa5ced83c606575cdd
AnthonyMerive <87150629+AnthonyMerive@users.noreply.github.com>
2023-04-20 22:08:23 -0500

    se creo la interfaz grafica, y creo codigo para visualizar el nombre.

--- app/src/main/java/com/example/ubicacion_personal/MainActivity.java
+++ app/src/main/java/com/example/ubicacion_personal/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.ubicacion_personal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     et1= (EditText) findViewById(R.id.txt_nom);
+     tv1 = (TextView) findViewById(R.id.txt_1);
+
+    }
+    public void mostarUbicacion(View view){
+        String nombre = et1.getText().toString();
+        String mensaje = "hola  " + nombre; tv1.setText(mensaje);
     }
 }
