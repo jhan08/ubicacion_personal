45c46aae3f1359f0900ef2d1ccf9f2659e846f2d
Alexander Sandoval <alexander.sf0828@gmail.com>
2023-04-21 20:10:47 -0500

    feat: get and show location and date

--- app/src/main/java/com/example/ubicacion_personal/MainActivity.java
+++ app/src/main/java/com/example/ubicacion_personal/MainActivity.java
@@ -1,25 +1,82 @@
 package com.example.ubicacion_personal;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private EditText et1;
     private TextView tv1;
+
+    private String pais;
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-     et1= (EditText) findViewById(R.id.txt_nom);
-     tv1 = (TextView) findViewById(R.id.txt_1);
+        et1 = (EditText) findViewById(R.id.txt_nom);
+        tv1 = (TextView) findViewById(R.id.txt_1);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
-    public void mostarUbicacion(View view){
+
+    protected void onResume() {
+        super.onResume();
+
+        obtenerUbicacion();
+    }
+
+    private void obtenerUbicacion() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 99);
+        }
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null){
+                            try {
+                                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                                pais = addresses.get(0).getCountryName();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                });
+    }
+
+    public void mostarUbicacion(View view) {
         String nombre = et1.getText().toString();
-        String mensaje = "hola  " + nombre; tv1.setText(mensaje);
+        String hora = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
+
+        String mensaje = "Hola " + nombre + " estas en " + pais + " y son las " + hora + ".";
+        tv1.setText(mensaje);
     }
 }
